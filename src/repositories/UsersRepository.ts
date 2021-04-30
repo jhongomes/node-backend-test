2f9bf8b31bd494bcf10c31bfcd85b39cb0758c45
jhongomes <jhonatangomes859@gmail.com>
2021-04-30 02:03:07 -0400

    Implementando UsersRepository

---
+++ src/repositories/UsersRepository.ts
@@ -0,0 +1,50 @@
+import { IUsersRepository } from "../repositories/IUsersRepository";
+import { ICreateUsersDTO } from "../Dtos/ICreateUsersDTO";
+import { User } from "../entities/User";
+import { getRepository, Repository} from 'typeorm';
+
+
+class UsersRepository implements IUsersRepository{
+
+  constructor(private ormRepository: Repository<User>){
+
+    this.ormRepository = getRepository(User);
+  }
+
+ public async create({name, lastname, nickname, address, bio}: ICreateUsersDTO): Promise<User> {
+  const users = this.ormRepository.create({
+    name,
+    lastname,
+    nickname,
+    address,
+    bio,
+  });
+
+  await this.ormRepository.save(users);
+
+  return users;
+}
+
+  public async listar(): Promise<User[]> {
+    return this.ormRepository.find();
+
+  }
+
+  public async save(users: User): Promise<User> {
+    return this.ormRepository.save(users);
+
+  }
+
+  public async remove(users: User): Promise<User> {
+    return this.ormRepository.remove(users);
+
+  }
+
+  public async findById(id: string): Promise<User | undefined> {
+    return this.ormRepository.findOne({
+      id,
+    })
+  }
+
+}
+export { UsersRepository }
